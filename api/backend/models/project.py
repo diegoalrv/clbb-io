3ed649806b98502783e30883dbc083dbca5669dd
AlonsoDiCandia <alonsodiazcandia@gmail.com>
2023-11-05 12:54:12 -0300

    Add project model

--- api/backend/models/project.py
+++ api/backend/models/project.py
@@ -1,13 +1,15 @@
 from django.db import models
 from uuid import uuid4
-from .project import Project
 
-class Slot(models.Model):
+class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=500)
-    number = models.PositiveIntegerField(default=0)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-
+    streets = models.BooleanField()
+    squares = models.BooleanField()
+    land_use = models.BooleanField()
+    green_area = models.BooleanField()
+    amenities = models.BooleanField()
+    
     def __str__(self):
         return f"Slot {self.name}"
 
