b69d46ff6bd264312836c816ddcb831f5bc74c1b
diegoalrv <d.alexis.ramirez@gmail.com>
2023-11-14 22:50:57 -0300

    update on street class

--- container/start_container.bat
+++ container/start_container.bat
@@ -1,20 +1,23 @@
 @echo off
 
-set "ruta_host1=C:\Users\CityLab Biobio - DS\Dev\clbb-io\data"
+set "ruta_base=C:\Users\dalex\Documents"
+
+set "ruta_host1=%ruta_base%\clbb-io\data"
 set "ruta_contenedor1=/app/data"
 
-set "ruta_host1=C:\Users\CityLab Biobio - DS\Dev\clbb-io\modules"
-set "ruta_contenedor1=/app/modules"
+set "ruta_host2=%ruta_base%\clbb-io\modules"
+set "ruta_contenedor2=/app/modules"
 
-set "ruta_host2=C:\Users\CityLab Biobio - DS\Dev\clbb-io\assets"
-set "ruta_contenedor2=/app/assets"
+set "ruta_host3=%ruta_base%\clbb-io\assets"
+set "ruta_contenedor3=/app/assets"
 
-set "nombre_imagen=urban_indicators"
+set "nombre_imagen=ui_clbb"
 set "nombre_contenedor=clbb-io-modules"
 
 docker run -d -p 9090:9090 ^
   -v "%ruta_host1%:%ruta_contenedor1%" ^
   -v "%ruta_host2%:%ruta_contenedor2%" ^
+  -v "%ruta_host3%:%ruta_contenedor3%" ^
   -e JUPYTER_TOKEN="" ^
   --name "%nombre_contenedor%" ^
   "%nombre_imagen%"
--- modules/Base.py
+++ modules/Base.py
@@ -1,9 +1,27 @@
 import pandas as pd
 import geopandas as gpd
 import numpy as np
-
+import glob
+import os
 
 class BaseModule():
     def __init__(self) -> None:
         self.default_crs = '32718'
+        self.load_plates()
+        self.load_area_scope()
+        pass
+
+    def load_plates(self):
+        plates_path = '/app/assets/plates'
+        plates_files = glob.glob(os.path.join(plates_path, '*'))
+        self.plates = {}
+        for file in plates_files:
+            idx = os.path.split(file)[-1]
+            plate = gpd.read_file(file).to_crs(self.default_crs)
+            self.plates[idx] = plate
+        pass
+
+    def load_area_scope(self):
+        area_scope_path = '/app/assets/area_scope'
+        self.area_scope = gpd.read_file(area_scope_path)
         pass
