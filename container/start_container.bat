b83887e0d49e113732adb1fb985b2fe76ea3777c
Diego Ramirez <d.alexis.ramirez@gmail.com>
2023-11-15 11:12:59 -0300

    update street class to change part of the network between scenarios

--- container/start_container.bat
+++ container/start_container.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "ruta_base=C:\Users\dalex\Documents"
+set "ruta_base=C:\Users\CityLab Biobio - DS\Dev"
 
 set "ruta_host1=%ruta_base%\clbb-io\data"
 set "ruta_contenedor1=/app/data"
@@ -11,7 +11,7 @@ set "ruta_contenedor2=/app/modules"
 set "ruta_host3=%ruta_base%\clbb-io\assets"
 set "ruta_contenedor3=/app/assets"
 
-set "nombre_imagen=ui_clbb"
+set "nombre_imagen=urban_indicators"
 set "nombre_contenedor=clbb-io-modules"
 
 docker run -d -p 9090:9090 ^
