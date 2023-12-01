3718fcbd1bc2d8ec7342b259d3486a99b1e92981
AlonsoDiCandia <alonsodiazcandia@gmail.com>
2023-12-01 16:36:02 -0300

    Filter by number when search slots

--- api/backend/views/slots/slots.py
+++ api/backend/views/slots/slots.py
@@ -4,7 +4,25 @@ from backend.serializers.map import SlotSerializer
 
 from django.conf import settings
 
-print(settings.BASE_DIR)
 class SlotViewSet(viewsets.ModelViewSet):
-    queryset = Slot.objects.all()
     serializer_class = SlotSerializer
+    queryset = Slot.objects.all()
+
+    # Definir el campo de búsqueda como 'number' en lugar de 'id'
+    lookup_field = 'number'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Verificar si se está accediendo al detalle o al listado
+        if self.action == 'list':
+            return queryset  # Listado completo
+        else:
+            # Acceso al detalle por 'number'
+            number_param = self.kwargs.get('number', None)
+            if number_param is not None:
+                queryset = queryset.filter(number=number_param)
+            
+            return queryset
+
+    
