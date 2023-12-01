5821649c27bd0ea592b563b09ee10713309c6f7f
AlonsoDiCandia <alonsodiazcandia@gmail.com>
2023-12-01 13:07:41 -0300

    Littles changes to avoid some bugs

--- camera/video_camera/capture_steps.py
+++ camera/video_camera/capture_steps.py
@@ -3,12 +3,13 @@ from capture_aruco import detectar_aruco
 from slider import Slider
 from utils import up_right_point, up_left_point
 
-def draw_horizontal_lines(frame, cords):
+def draw_horizontal_lines(frame, cords, start_comparing = False):
     cord_1 = up_left_point(cords[0])
     cord_2 = up_right_point(cords[1])
 
-    slider.slider_line = cord_1, cord_2
-    
+    if not start_comparing:
+        slider.slider_line = cord_1, cord_2
+
     # Calcular puntos finales de la línea en el borde izquierdo y derecho de la imagen
     x_left = cord_1[0]
     y_left = cord_1[1]
@@ -28,6 +29,9 @@ img_path = "/Users/alonsodicandia/asesorias/clbb-io/camera/images/capture/imagen
 slider = Slider()
 line_color = (0, 255, 0)
 middle_line_color = (0, 0, 255)
+min_distance_color = (255, 0, 0)
+
+position = ['1','2','3','4']
 
 while True:
     # Captura el fotograma
@@ -55,16 +59,24 @@ while True:
                 line_division = slider.subdividir_recta(p1,p2)
 
                 print(line_division)
+                distances = []
 
                 for point in line_division:
                     x, y = point
+                    distance = slider.calcular_distancia(point)
+                    distances.append(distance)
+
                     # print(x,y)
                     cv2.line(frame, slider.center_one_aruco, (x,y), middle_line_color, 2)
+                
+                index_min_distance = distances.index(min(distances))
+                cv2.line(frame, slider.center_one_aruco, line_division[index_min_distance], min_distance_color, 2)
+                cv2.putText(frame, position[index_min_distance], (1400,300), cv2.FONT_HERSHEY_SIMPLEX, 1, min_distance_color, 5)
             except Exception as e:
                 print(e)
             cv2.imshow("Video con aruco", frame)
         elif len(ids) == 2:
-            images_with_circles = draw_horizontal_lines(frame, corners)
+            images_with_circles = draw_horizontal_lines(frame, corners, START_COMPARING)
             cv2.imshow("Video con aruco", images_with_circles)
         else:
             cv2.imshow("Video con aruco", image_with_aruco_marker)
--- camera/video_camera/slider.py
+++ camera/video_camera/slider.py
@@ -6,6 +6,10 @@ class Slider:
         self._left_point = (None, None)
         self._right_point = (None, None)
 
+        self.__first_section = (None, None)
+        self.__second_section = (None, None)
+        self.__third_section = (None, None)
+
         # reference to a center of the just one aruco marker, reference to the slider aruco
         self.center = None
 
@@ -31,6 +35,14 @@ class Slider:
         right = two_points[1]
         self._left_point = left
         self._right_point = right
+
+    def __save_division_points(self, p1, p2, d1, d2, d3):
+        self._left_point = p1
+        self._right_point = p2
+
+        self.__first_section = d1
+        self.__second_section = d2
+        self.__third_section = d3
     
     def process_upper_corners_to_get_inclination(self, corner):
         up_left = up_left_point(corner)
@@ -69,26 +81,67 @@ class Slider:
 
     def subdividir_recta(self, point1, point2):
         def obtener_menor_punto(tupla1, tupla2):
-            x = min([tupla1, tupla2], key=lambda punto: punto[0])
-            y = min([tupla1, tupla2], key=lambda punto: punto[1])
+            # x = min([tupla1, tupla2], key=lambda punto: punto[0])
+            # y = min([tupla1, tupla2], key=lambda punto: punto[1])
 
-            return x[0], y[1]
-            
+            x1, y1 = tupla1
+            x2, y2 = tupla2
+
+            xr, yr = None, None
+
+            if x1 < x2:
+                xr = x1
+                yr = y1
+            elif x2 < x1:
+                xr = x2
+                yr = y2
+            else:
+                xr = x1
+                yr = y1
+                
+            return xr, yr
+        
         def make_int_tuple(t):
             return (int(t[0]), int(t[1]))
 
         # Calcular el point medio de la recta
         middle_point = ((point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2)
+
+
+
         print(point1, point2)
-        point1 = obtener_menor_punto(point1, point2)
+
+
+        work_point = obtener_menor_punto(point1, point2)
+
+        direction = 1
+
+        if work_point == point1:
+            if work_point[1] > point2[1]:
+                direction = -1
+        elif work_point == point2:
+            if work_point[1] > point1[1]:
+                direction = -1
+
         print(f"middle: {middle_point}")
-        print(f"Menor: {point1}")
+        print(f"Menor: {work_point}")
+
+        
 
         # Calcular dos points adicionales equidistantes
-        first_point = ((point1[0] + (abs(point1[0] - middle_point[0]) / 4)),(point1[1] + (abs(point1[1] - middle_point[1]) / 4)))
-        second_poitn = ((point1[0] + (3 * abs(point1[0] - middle_point[0]) / 4)),(point1[1] + (3 * abs(point1[1] - middle_point[1]) / 4)))
-        third_point = ((point1[0] + (5 * abs(point1[0] - middle_point[0]) / 4)),(point1[1] + (5 * abs(point1[1] - middle_point[1]) / 4)))
-        fourth_point = ((point1[0] + (7 * abs(point1[0] - middle_point[0]) / 4)),(point1[1] + (7 * abs(point1[1] - middle_point[1]) / 4)))
+        first_point = ((work_point[0] + ((abs(work_point[0] - middle_point[0]) / 4))),(work_point[1] + (direction * abs(work_point[1] - middle_point[1]) / 4)))
+        second_poitn = ((work_point[0] + (3 * abs(work_point[0] - middle_point[0]) / 4)),(work_point[1] + (3 * direction * abs(work_point[1] - middle_point[1]) / 4)))
+        third_point = ((work_point[0] + (5 * abs(work_point[0] - middle_point[0]) / 4)),(work_point[1] + (5 * direction * abs(work_point[1] - middle_point[1]) / 4)))
+        fourth_point = ((work_point[0] + (7 * abs(work_point[0] - middle_point[0]) / 4)),(work_point[1] + (7 * direction * abs(work_point[1] - middle_point[1]) / 4)))
 
         return make_int_tuple(first_point), make_int_tuple(second_poitn), make_int_tuple(third_point), make_int_tuple(fourth_point)
+    
+    def calcular_distancia(self, punto1):
+        punto2 = self.center_one_aruco
+        
+        x1, y1 = punto1
+        x2, y2 = punto2
+
+        distancia = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        return distancia
 
