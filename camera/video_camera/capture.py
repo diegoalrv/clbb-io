cf16d734e91e51c51e8c1fb55dd363a23dc0acbb
AlonsoDiCandia <alonsodiazcandia@gmail.com>
2023-12-01 18:31:57 -0300

    Show map using camera and backend

--- camera/video_camera/capture.py
+++ camera/video_camera/capture.py
@@ -1,8 +1,10 @@
 import cv2
-import time
+import requests
+import numpy as np
 from capture_aruco import detectar_aruco
 from slider import Slider
 from utils import up_right_point, up_left_point
+from io import BytesIO
 
 def draw_horizontal_lines(frame, cords, start_comparing = False):
     cord_1 = up_left_point(cords[0])
@@ -43,23 +45,38 @@ while True:
     image_with_aruco_marker, corners, ids = detectar_aruco(frame.copy())
 
     if ids is not None:
+        for id in ids:
+            if id >= 3:
+                index = ids.index(id)
+                esquina_superior_izquierda = corners[index][0][0]
+                esquina_inferior_derecha = corners[index][0][2]
+                centro_x = int((esquina_superior_izquierda[0] + esquina_inferior_derecha[0]) // 2)
+                centro_y = int((esquina_superior_izquierda[1] + esquina_inferior_derecha[1]) // 2)
+
+                # Especifica la URL a la que deseas hacer la solicitud GET
+                url = f'http://localhost:8500/api/slots/{id}/'
+
+                # Realiza la solicitud GET
+                response = requests.get(url)
+
+                # Verifica si la solicitud fue exitosa (código de estado 200)
+                if response.status_code == 200:
+                    # Imprime el contenido de la respuesta
+                    data = response.json()
+                    cv2.putText(frame, data["name"], (centro_x,centro_y), cv2.FONT_HERSHEY_SIMPLEX, 2, middle_line_color, 3)
+
         if 0 in ids and START_COMPARING:
             p1, p2 = slider.slider_line
 
             cv2.line(frame, p1, p2, line_color, 2)
 
-            # print(corners, ids)
-            # break
             index_aruco_zero = ids.index(0)
-            # print(index_aruco_zero)
             esquina_superior_izquierda = corners[index_aruco_zero][0][0]
             esquina_inferior_derecha = corners[index_aruco_zero][0][2]
             centro_x = int((esquina_superior_izquierda[0] + esquina_inferior_derecha[0]) // 2)
             centro_y = int((esquina_superior_izquierda[1] + esquina_inferior_derecha[1]) // 2)
 
             slider.center_one_aruco = (centro_x, centro_y)
-            # print(slider.center_one_aruco)
-            # slider.process_upper_corners_to_get_inclination(corners[0])
 
             try:
                 line_division = slider.subdividir_recta(p1,p2)
@@ -76,11 +93,9 @@ while True:
                 
                 index_min_distance = distances.index(min(distances))
                 cv2.line(frame, slider.center_one_aruco, line_division[index_min_distance], min_distance_color, 2)
-                cv2.putText(frame, position[index_min_distance], (1400,300), cv2.FONT_HERSHEY_SIMPLEX, 1, min_distance_color, 5)
             except Exception as e:
                 print(e)
             cv2.imshow("Video con aruco", frame)
-            # continue
         elif 1 in ids and 2 in ids and not START_COMPARING:
             images_with_circles = draw_horizontal_lines(frame, corners)
             cv2.imshow("Video con aruco", images_with_circles)
@@ -90,12 +105,31 @@ while True:
     else:
         cv2.imshow("Video con aruco", image_with_aruco_marker)
 
+    
+    # Finally
+    if START_COMPARING:
+        try:
+            url = f'http://localhost:8500/api/maps/?slider={position[index_min_distance]}&slot={data["number"]}'
+            # Realiza la solicitud GET
+            response = requests.get(url)
+
+            # Verifica si la solicitud fue exitosa (código de estado 200)
+            if response.status_code == 200:
+                # Imprime el contenido de la respuesta
+                data = response.json()
+                data = data[0]
+                image_url = data["image"]
+                response = requests.get(image_url)
+                if response.status_code == 200:
+                    image_data = BytesIO(response.content)
+                    map_image = cv2.imdecode(np.frombuffer(image_data.read(), np.uint8), cv2.IMREAD_COLOR)
+                    cv2.imshow('Map image', map_image)
+        except:
+            print("No hay endpoint")
     # Rompe el bucle si se presiona la tecla 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.imwrite(img_path, frame)
         break
-    elif cv2.waitKey(1) & 0xFF == ord('s'):
-        START_COMPARING = True
 
 # Libera la cámara y cierra la ventana
 cap.release()
