4599ede96c80c37cf11b3094e790ce9b8b22ccc7
AlonsoDiCandia <alonsodiazcandia@gmail.com>
2023-12-06 17:55:38 -0300

    Extrapole Slider to work with even sections

--- camera/video_camera/capture.py
+++ camera/video_camera/capture.py
@@ -1,29 +1,38 @@
 import cv2
 import requests
 import numpy as np
-from capture_aruco import detectar_aruco
-from slider import Slider
-from utils import up_right_point, up_left_point
+from camera.aruco.capture_aruco import detectar_aruco
+from camera.slider.slider import Slider
 from io import BytesIO
 
-def draw_horizontal_lines(frame, cords, start_comparing = False):
-    cord_1 = up_left_point(cords[0])
-    cord_2 = up_right_point(cords[1])
+def calculate_center(cord):
+    esquina_superior_izquierda = cord[0][0]
+    esquina_inferior_derecha = cord[0][2]
+    centro_x = int((esquina_superior_izquierda[0] + esquina_inferior_derecha[0]) // 2)
+    centro_y = int((esquina_superior_izquierda[1] + esquina_inferior_derecha[1]) // 2)
+
+    return (centro_x, centro_y)
+
+def draw_horizontal_lines(frame, cord1, cord2, start_comparing = False):
+    
+    cord_1 = calculate_center(cord1)
+    cord_2 = calculate_center(cord2)
 
     if not start_comparing:
         slider.slider_line = cord_1, cord_2
 
     # Calcular puntos finales de la línea en el borde izquierdo y derecho de la imagen
-    x_left = cord_1[0]
-    y_left = cord_1[1]
-    x_right = cord_2[0]
-    y_right = cord_2[1]
+    x_1 = cord_1[0]
+    y_1 = cord_1[1]
+    x_2 = cord_2[0]
+    y_2 = cord_2[1]
     
     # Dibujar la línea
-    frame = cv2.line(frame, (x_left, y_left), (x_right, y_right), (0, 255, 0), 2)
+    frame = cv2.line(frame, (x_1, y_1), (x_2, y_2), (0, 255, 0), 2)
     return frame
 
 START_COMPARING = False
+NUM_DIVISION = 8
 
 # Abre la cámara (usualmente la cámara predeterminada, 0)
 cap = cv2.VideoCapture(0)
@@ -71,15 +80,12 @@ while True:
             cv2.line(frame, p1, p2, line_color, 2)
 
             index_aruco_zero = ids.index(0)
-            esquina_superior_izquierda = corners[index_aruco_zero][0][0]
-            esquina_inferior_derecha = corners[index_aruco_zero][0][2]
-            centro_x = int((esquina_superior_izquierda[0] + esquina_inferior_derecha[0]) // 2)
-            centro_y = int((esquina_superior_izquierda[1] + esquina_inferior_derecha[1]) // 2)
+            centro_x, centro_y = calculate_center(corners[index_aruco_zero])
 
             slider.center_one_aruco = (centro_x, centro_y)
 
             try:
-                line_division = slider.subdividir_recta(p1,p2)
+                line_division = slider.subdividir_recta(p1,p2, NUM_DIVISION)
 
                 distances = []
 
@@ -97,9 +103,16 @@ while True:
                 print(e)
             cv2.imshow("Video con aruco", frame)
         elif 1 in ids and 2 in ids and not START_COMPARING:
-            images_with_circles = draw_horizontal_lines(frame, corners)
-            cv2.imshow("Video con aruco", images_with_circles)
-            START_COMPARING = True
+            index_aruco_1 = ids.index(1)
+            index_aruco_2 = ids.index(2)
+            try:
+                images_with_circles = draw_horizontal_lines(frame, corners[index_aruco_1], corners[index_aruco_2])
+                cv2.imshow("Video con aruco", images_with_circles)
+                START_COMPARING = True
+            except:
+                pass
+
+
         else:
             cv2.imshow("Video con aruco", image_with_aruco_marker)
     else:
@@ -108,6 +121,7 @@ while True:
     
     # Finally
     if START_COMPARING:
+        response = None
         try:
             url = f'http://localhost:8500/api/maps/?slider={position[index_min_distance]}&slot={data["number"]}'
             # Realiza la solicitud GET
